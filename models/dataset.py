bcfc3fcd5a3bbd7760111fd8ab1b858b42b965fc
Garrett <gwink00@gmail.com>
2026-09-17 14:08:37 -0500

    load df in script

--- models/dataset.py
+++ models/dataset.py
@@ -6,4 +6,9 @@ conn = sqlite3.connect("nba.db")
 df_stat = pd.read_sql_query("SELECT * FROM player_season_history", conn)
 df_award = pd.read_sql_query("SELECT * FROM mvp_awards", conn)
 
-print(df_award.head())
+df = df_stat.merge(
+    df_award[['player_id', 'season', 'mvp_rank', 'points_won', 'mvp_vote_share']],
+    on=['player_id', 'season'],
+    how='left'
+)
+
